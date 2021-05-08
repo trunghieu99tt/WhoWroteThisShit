6c1558324654f86634d1da095826c2be86828e90
Nguyen Trung Hieu <trunghieu99tt@gmail.com>
2021-05-08 16:28:02 +0700

    initial commit

--- site.config.js
+++ site.config.js
@@ -1,51 +1,53 @@
 module.exports = {
-  // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+    // where it all starts -- the site's root Notion page (required)
+    // rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+    rootNotionPageId: '16dc2d2fdc8c46ee96f581afde5a0c0c',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+    // if you want to restrict pages to a single notion workspace (optional)
+    // (this should be a Notion ID; see the docs for how to extract this)
+    rootNotionSpaceId: null,
 
-  // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+    // basic site info (required)
+    name: 'Code This Shit',
+    domain: 'rikikudo.it',
+    author: 'Rikikudo',
 
-  // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+    // open graph metadata (optional)
+    description:
+        'This is a blog site which contains all blogs written by Nguyen Trung Hieu (aka Rikikudo)',
+    socialImageTitle: 'Code This Shit',
+    socialImageSubtitle: 'Hello World! 👋',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+    // social usernames (optional)
+    facebook: 'rikikudo99',
+    github: 'trunghieu99tt',
+    linkedin: 'rikikudo',
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
+    // default notion icon and cover images for site-wide consistency (optional)
+    // page-specific values will override these site-wide defaults
+    defaultPageIcon: null,
+    defaultPageCover: null,
+    defaultPageCoverPosition: 0.5,
 
-  // image CDN host to proxy all image requests through (optional)
-  // NOTE: this requires you to set up an external image proxy
-  imageCDNHost: null,
+    // image CDN host to proxy all image requests through (optional)
+    // NOTE: this requires you to set up an external image proxy
+    imageCDNHost: null,
 
-  // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
+    // Utteranc.es comments via GitHub issue comments (optional)
+    utterancesGitHubRepo: null,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  // NOTE: this requires you to set up Google Firebase and add the environment
-  // variables specified in .env.example
-  isPreviewImageSupportEnabled: false,
+    // whether or not to enable support for LQIP preview images (optional)
+    // NOTE: this requires you to set up Google Firebase and add the environment
+    // variables specified in .env.example
+    isPreviewImageSupportEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null
-}
+    // map of notion page IDs to URL paths (optional)
+    // any pages defined here will override their default URL paths
+    // example:
+    //
+    // pageUrlOverrides: {
+    //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+    //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+    // }
+    pageUrlOverrides: null
+};
