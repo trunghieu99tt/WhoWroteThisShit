57f980e180c578ad014505dd3ed7e3acff2985c2
Elliot Nguyen <trunghieu99tt@gmail.com>
2025-01-04 15:06:49 +0700

    chore: update copyright year

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -31,10 +31,12 @@ const Footer: React.FC<{
         setHasMounted(true);
     }, []);
 
+    const copyRightYear = new Date().getUTCFullYear();
+
     return (
         <footer className={styles.footer}>
             <div className={styles.copyright}>
-                Copyright 2024 @{config.author}
+                Copyright {copyRightYear} @{config.author}
             </div>
 
             {hasMounted ? (
